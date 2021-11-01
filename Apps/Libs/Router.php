35cc0cdd2183fe3e0d48c1952e4261a6bad4e18e
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2021-11-01 21:59:03 +0700

    ls38

---
+++ Apps/Libs/Router.php
@@ -0,0 +1,53 @@
+<?php
+class Apps_Libs_Router
+{
+    const PARAM_NAME = 'r';
+    const HOME_PAGE = 'home';
+    const INDEX_PAGE = 'index';
+
+    public static $sourcePath;
+
+    public function __construct($sourcePath = '')
+    {
+        if ($sourcePath) {
+            self::$sourcePath = $sourcePath;
+        }
+    }
+
+    public function getGET($name = null)
+    {
+        if ($name != null) {
+            return isset($_GET[$name]) ? $_GET[$name] : null;
+        }
+        return $_GET;
+    }
+
+    public function getPOST($name = null)
+    {
+        if ($name != null) {
+            return isset($_POST[$name]) ? $_POST[$name] : null;
+        }
+        return $_POST;
+    }
+
+    public function router()
+    {
+        $url = $this->getGET(self::PARAM_NAME);
+        if (!is_string($url) || !$url || $url===self::INDEX_PAGE) {
+            $url = self::HOME_PAGE;
+        } else {
+            $path = self::$sourcePath . '/' . $url.'.php';
+        }
+        if (file_exists($path)) {
+            return require_once $path;
+        } else {
+            $this->pageNotFound();
+        }
+    }
+
+    public function pageNotFound()
+    {
+        echo 'Page not found 404';
+        die();
+    }
+}
--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,5 @@
 <?php
 include '../apps/bootstrap.php';
 
-$a = new Apps_Models_Users();
-$result = $a->buildQueryParams([
-    "field"=>"(username,password) values (?,?)",
-            "value"=>["admin",md5("admin")]
-])->insert();
-var_dump($result);
-// foreach ($result as $key => $value) {
-//     foreach ($value as $key => $field) {
-//         echo $key.': '.$field.'<br>';
-//     }
-// }
+$router =new Apps_Libs_Router(__DIR__);
+$router->router();
